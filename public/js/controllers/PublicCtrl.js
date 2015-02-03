2995c531143b1c06adedf928020333cf344d433d
winklenurfer@gmail.com <alex.dickson@blackboard.com>
2015-02-03 00:56:24 -0800

    adding colors.css and other style changes, updating public page template

--- public/js/controllers/PublicCtrl.js
+++ public/js/controllers/PublicCtrl.js
@@ -1,5 +1,18 @@
-angular.module('PublicCtrl', []).controller('PublicController', function($scope) {
+angular.module('PublicCtrl', []).controller('PublicController', function($scope, $location) {
 
-	$scope.tagline = 'Public';
+	$scope.openElections = [
+        {"name":"Awesome Election", "id":12345, "time": "11:35 PM 02/03/15"},
+        {"name":"Rockin' Poll", "id":56789, "time": "07:20 AM 10/31/14"},
+        {"name":"Cash Money", "id":19283, "time": "12:00 PM 01/01/15"}
+    ];
+
+    $scope.closedElections = [
+        {"name":"Terrible Election", "id":54321, "time": "11:35 PM 02/03/15"},
+        {"name":"No Money", "id":38291, "time": "12:00 PM 01/01/15"}
+    ];
+
+    $scope.openElection = function(election) {
+        $location.path('/election/' + election.id);
+    };
 
 });
